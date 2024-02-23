a03a3a6c28ade921815497f20815221de8cd6b43
jonnnnbrito <jonnnnbrito@gmail.com>
2024-02-23 13:58:09 +0800

    Added PositiveNEgativeZeroCheck

---
+++ PositiveNegativeZeroCheck/src/PositiveNegativeZeroCheck.java
@@ -0,0 +1,14 @@
+public class PositiveNegativeZero {
+    // write code here
+    public static void checkNumber (int number) {
+        if (number > 0) {
+            System.out.print("positive");
+        } else if (number < 0) {
+            System.out.print("negative");
+        } else {
+            System.out.print("zero");
+        }
+
+    }
+
+}
